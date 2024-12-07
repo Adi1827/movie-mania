67c6418f7765efd447670bf1bb66ad0c86d24de9
Aditya Dalwadi <102174643+Adi1827@users.noreply.github.com>
2024-12-07 20:53:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        default: '5px 5px 5px 5px #00010080',
+        hover: '15px 15px 15px 10px #00010080',
+        card: '5px 5px 5px 5px rgba(0,0,0,.5);',
+      },
+      fontFamily: {
+        sans: ["system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Open Sans", "Helvetica Neue", "sans-serif"],
+      },
+      backgroundColor: {
+        'primary': '#948979'
+      },
+      
+    },
+  },
+  plugins: [],
+}
