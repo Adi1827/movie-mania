5934d577701b92164877069c91a0302fa914d890
Aditya Dalwadi <102174643+Adi1827@users.noreply.github.com>
2024-12-11 15:29:01 +0530

    Update Moviecard.js

--- src/Components/Moviecard.js
+++ src/Components/Moviecard.js
@@ -18,7 +18,7 @@ const MovieCard = ({...movieItem}) => {
     }
     return(
         <div className="w-[250px] h-[600px] mt-5 mr-[6px] shadow-default overflow-hidden rounded-lg ease-in-out hover:shadow-hover hover:[text-shadow:1px_0_10px_#FFF] duration-200">
-            <img src={(movieItem.poster_path!== undefined) ? "https://image.tmdb.org/t/p/w500/"+movieItem?.poster_path : posterHolder} className='w-full h-[400px] m-auto block ease-in-out hover:shadow-card duration-300' alt="Not found" />
+            <img src={(movieItem.poster_path !== null && movieItem.poster_path !== undefined) ? "https://image.tmdb.org/t/p/w500/"+movieItem?.poster_path : posterHolder} className='w-full h-[400px] m-auto block ease-in-out hover:shadow-card duration-300' alt="Not found" />
             <h2 className="text-center text-[#483C46] p-2.5 text-2xl font-extrabold overflow-clip">{movieItem.title ? movieItem.title : movieItem.name}</h2>
             {/* <h2 className="text-center text-[#483C46] p-2.5 text-2xl font-extrabold overflow-clip">{movieItem?.release_date?.split('-')[0]}</h2>
             <h4 className="text-center text-[#483C46] p-2.5 text-2xl font-extrabold overflow-clip">{movieGenre?.map((genre)=>{return genre.name}).join(', ')}</h4> */}
